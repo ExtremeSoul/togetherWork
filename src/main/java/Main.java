1766c17eb4dc752660b340edaf10e58361136805
ExtremeSoul <clegutko@gmail.com>
2017-12-16 15:17:28 +0100

    Working progress

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,22 @@
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+
 public class Main {
+    private static final int NUMBER_COUNTER = 10;
     public static void main(String[] args) {
-        
+
+        piRandom();
+    }
+
+    public static void piRandom(){
+        Random random = new Random(10);
+        int decimalPlaces = random.nextInt();
+
+        for (int i = 0; i < NUMBER_COUNTER ; i++) {
+
+            String.format("%1$,.2f", decimalPlaces, Math.PI);
+
+        }
     }
 }
