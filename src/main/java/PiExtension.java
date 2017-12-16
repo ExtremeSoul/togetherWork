ab1bafa5b04f5aa8aa68b7a6c36aaa9d396084b3
Bartlomiej <bartlomiejgluch@gmail.com>
2017-12-16 15:29:08 +0100

    Add method getPi

--- src/main/java/PiExtension.java
+++ src/main/java/PiExtension.java
@@ -3,6 +3,11 @@ import java.util.IllegalFormatPrecisionException;
 public class PiExtension {
 
 
+    public static String getPi(int x) {
 
-    
+        double a = Math.PI;
+
+        return String.format("%."+x+"f%n", a);
+    }
+
 }
